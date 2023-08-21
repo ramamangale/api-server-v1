edbda53d0a0783cff3eeb3a2a1250f2c3dab977c
coder4509 <60377831+coder4509@users.noreply.github.com>
2023-08-21 17:04:55 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,8 +4,11 @@ const path = require('path');
 
 app.use(express.static('./public'));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve('./pages/index.html'));
+app.get('/api', (req, res) => {
+  const path = `Test`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
 });
 
 module.exports = app;
